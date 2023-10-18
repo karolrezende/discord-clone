9edfbb2dbf0c2bb1f1cbafa24cfa08f196ee3086
karol <karolrds01@gmail.com>
2023-10-18 16:13:05 -0400

    add notes

--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import "@uploadthing/react/styles.css"
 import { UploadDropzone } from '@uploadthing/react'
 
+import { X } from 'lucide-react'
+import  Image from 'next/image'
+
 interface FileUploadThing {
     onChange: (url?: string) => void
     value: string
     endpoint: "messageFile" | "serverImage"
 }
 export default function FileUpload({onChange, value, endpoint}: FileUploadThing) {
+  const fileType = value.split('.').pop() // irá pegar o tipo que vem "serverImage", separar por um ponto e retirar
+  
+  if(value && fileType !== "pdf"){
+    return(
+      <div className='relative h-20 w-20' >
+        <Image fill src={value} alt="Upload" className='rounded-full'/>
+      </div>
+    )
+  }
+
   return (
-    <UploadDropzone endpoint={endpoint} onClientUploadComplete={(res)=> { onChange(res?.[0].url)}} onUploadError={(error: Error)=> console.log(error)}/>
+    <UploadDropzone  className='rounded-full' endpoint={endpoint} onClientUploadComplete={(res)=> { onChange(res?.[0].url)}} onUploadError={(error: Error)=> console.log(error)}/>
   )
 }
   
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    imagens:{
+        domains: [
+            "uploadthing.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
