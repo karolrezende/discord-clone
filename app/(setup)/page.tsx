6da55e112a4e156537a4e794d9e1d2f95f1cdd16
karol <karolrds01@gmail.com>
2023-10-18 02:02:22 -0400

    add profile

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,24 @@
+import { db } from '@/lib/database'
+import { initialProfile } from '@/lib/initial-profile'
+import {redirect} from "next/navigation"
 import React from 'react'
 const Home = async() => {
+  const profile = await initialProfile()
+
+  const servers = await db.server.findFirst({
+    where:{
+      members:{
+        some:{
+          profileId: profile.id
+        }
+      }
+    }
+  })
+  if(servers){
+    return redirect(`/servers/${servers.id}`)
+  }
   return (
-    <div>Criar uma conta</div>
+    <div>Criar perfil</div>
   )
 }
 export default Home
--- lib/initial-profile.tsx
+++ lib/initial-profile.tsx
@@ -10,5 +10,23 @@ export const initialProfile = async()=> {
         return redirectToSignIn()
     }
 
-    const profile = await db.profile
+    const profile = await db.profile.findUnique({
+        where:{
+            userId: user.id
+        }
+    })
+
+    if (profile){
+        return profile
+    }
+    
+    const newProfile = await db.profile.create({
+        data: {
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    })
+    return newProfile
 }
