acffe761720ae285d72157405ba0485c338dc65c
karol <karolrds01@gmail.com>
2023-10-24 19:23:13 -0300

    add upload-thing

--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -1,29 +1,77 @@
-import React from 'react'
-import "@uploadthing/react/styles.css"
+"use client";
+
+import { FileIcon, X } from "lucide-react";
+import Image from "next/image";
+
 import { UploadDropzone } from "@/app/api/uploadthing/uploadthing";
 
-import { X } from 'lucide-react'
-import  Image from 'next/image'
+import "@uploadthing/react/styles.css";
 
-interface FileUploadThing {
-  onChange: (url?: string) => void
-  value: string
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
   endpoint: "messageFile" | "serverImage"
 }
-export default function FileUpload({onChange, value, endpoint}: FileUploadThing) {
-  const fileType = value.split('.').pop() // irá pegar o tipo que vem "serverImage", separar por um ponto e retirar
-  
-  if(value && fileType !== "pdf"){
-    return(
-      <div className='relative h-20 w-20' >
-        <Image fill src={value} alt="Upload" className='rounded-full'/>
-        <button onClick={()=> onChange('')} className='bg'/>
-        <X className='h-4 w-4'/>
+
+export const FileUpload = ({
+  onChange,
+  value,
+  endpoint
+}: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+        />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a 
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     )
   }
 
   return (
-    <UploadDropzone  className='rounded-full' endpoint={endpoint} onClientUploadComplete={(res)=> { onChange(res?.[0].url)}} onUploadError={(error: Error)=> console.log(error)}/>
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
   )
 }
