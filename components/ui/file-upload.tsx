802222d58c92f0e1fe4e62cf43df732c82de4c28
karol <karolrds01@gmail.com>
2023-10-24 19:14:09 -0300

    add file-upload

--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -17,14 +17,14 @@ export default function FileUpload({onChange, value, endpoint}: FileUploadThing)
     return(
       <div className='relative h-20 w-20' >
         <Image fill src={value} alt="Upload" className='rounded-full'/>
-        <button onClick={()=> onChange('')} className='bg'/>
+        <button onClick={()=> onChange('')} className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'/>
         <X className='h-4 w-4'/>
       </div>
     )
   }
 
   return (
-    <UploadDropzone  className='rounded-full' endpoint={endpoint} onClientUploadComplete={(res)=> { onChange(res?.[0].url)}} onUploadError={(error: Error)=> console.log(error)}/>
+    <UploadDropzone className='rounded-full' endpoint={endpoint} onClientUploadComplete={(res)=> { onChange(res?.[0].url)}} onUploadError={(error: Error)=> console.log(error)}/>
   )
 }
   
