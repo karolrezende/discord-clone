22097bdb3d1b7c3801d2b2fe6401d3748eabeeef
karol <karolrds01@gmail.com>
2023-10-25 15:09:40 -0400

    started navitem

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,8 @@
+"use client"
+import React from 'react'
+
+export default function NavigationItem() {
+  return (
+    <div>NavigationItem</div>
+  )
+}
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,13 @@
-import { currentProfile } from '@/lib/current-profile'
-import { db } from '@/lib/database'
 import { redirect } from 'next/navigation'
 import React from 'react'
-import NavigationAction from './navigation-action'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/database'
 import { Separator } from '@/components/ui/separator'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+import NavigationAction from './navigation-action'
+import NavigationItem from './navigation-item'
 
 export default async function NavigationSidebar() {
   const profile = await currentProfile() //importa o perfil que está logado do banco
@@ -12,7 +16,7 @@ export default async function NavigationSidebar() {
     return redirect('/') //se não tiver nenhum perfil redireciona para pagina inicial
   }
 
-  const servers = db.server.findMany({
+  const servers = await db.server.findMany({
     where:{
       members:{
         some:{
@@ -22,9 +26,16 @@ export default async function NavigationSidebar() {
     }
   })
   return (
-    <div className='flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3'>
+    <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3'>
         <NavigationAction/>
-        <Separator/>
+        <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto'/>
+        <ScrollArea className='flex-1 w-full'>
+          {servers.map((server)=>(
+              <div key={server.id} className='mb-4'>
+                <NavigationItem/>
+              </div>
+            ))}
+        </ScrollArea>
     </div>
   )
 }
