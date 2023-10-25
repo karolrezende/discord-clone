11eff25e133c7ee3cda01276f702b4ee6d439603
karol <karolrds01@gmail.com>
2023-10-24 21:39:32 -0300

    add navside

---
+++ app/(main)/(route)/servers/[serverId]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ServerIdPage() {
+  return (
+    <div>ServerPage</div>
+  )
+}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NavigationSidebar() {
+  return (
+    <div>Navigation Sidebar</div>
+  )
+}
